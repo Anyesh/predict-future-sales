57026db87ce8f3c55cb2b1e9d038484e105aefb7
Anyesh <sthacruz@gmail.com>
2020-09-28 20:45:35 +0545

    Add get latest util

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import glob
 import itertools as IT
 import os
 import tempfile
@@ -45,3 +46,9 @@ def uniquify(path, sep=""):
         fd, filename = tempfile.mkstemp(dir=dirname, prefix=filename, suffix=ext)
         tempfile._name_sequence = orig
     return filename
+
+
+def get_latest_file(dir_path, file_type="*"):
+    list_of_files = glob.glob(os.path.join(dir_path, file_type))
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file

4ed0f6d70bcd2641e8b97cbd6ee4c9169677d3bf
Anyesh <sthacruz@gmail.com>
2020-09-29 10:28:56 +0545

    Fix for new data changes

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ def train_model(X_train, Y_train, X_validation, Y_validation, X_test, model_name
     model.fit(
         X_train,
         Y_train,
-        eval_set=[(X_validation, Y_validation)],
+        # eval_set=[(X_validation, Y_validation)],
         **model_params[model_name],
     )
 
@@ -53,19 +53,22 @@ def test_model(X_train, Y_train, X_validation, Y_validation, X_test, model):
 
 
 if __name__ == "__main__":
-    MODEL_NAME = "xgboost"
-    TRAIN_MODEL = False
+    MODEL_NAME = "linear_regression"
+    TRAIN_MODEL = True
+    IGNORE_CHECKPOINT = True
 
     fg = FeatureGenerator()
 
-    if no_files(CHK_PATH, ["frozen_data.npz"]):
-        X_train, Y_train, X_validation, Y_validation, X_test = fg.generate()
+    if no_files(CHK_PATH, ["frozen_data.npz"]) or IGNORE_CHECKPOINT:
+        X_train, Y_train, X_validation, Y_validation, test_data, cols = fg.generate()
+        logger.info(f"Final data columns {cols}")
+
         X_train, Y_train, X_validation, Y_validation, X_test = (
             X_train.values,
             Y_train.values,
             X_validation.values,
             Y_validation.values,
-            X_test.values,
+            test_data[cols].values,
         )
         np.savez(
             os.path.join(CHK_PATH, "frozen_data.npz"),
@@ -106,7 +109,7 @@ if __name__ == "__main__":
     test_predictions = test_model(
         X_train, Y_train, X_validation, Y_validation, X_test, model
     )
-    _, test_data = fg.get_datasets()
+
     prediction_df = pd.DataFrame(test_data["ID"], columns=["ID"])
     prediction_df["item_cnt_month"] = test_predictions.clip(0.0, 20.0)
     prediction_df.to_csv(
